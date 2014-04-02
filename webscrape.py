c30f90820a981b1c96d475d8a38e9bcfc4623899
NinJaSl0th <sl0th@force-sec.com>
2014-04-02 12:49:10 -0400

    Added comments and Trys
    Added some comments within the code. Also added a try for the items.dat
    file. That way if the user moves the script and not the file, it won’t
    continue to run and break.

--- webscrape.py
+++ webscrape.py
@@ -2,17 +2,24 @@
 
 import urllib2
 import re
-import readline
+import readline #nice when you need to use arrow keys and backspace
+import sys
 
 site = raw_input("Enter page: ")
 
 #open site. read so we can read in a string context
 data = urllib2.urlopen(site).read()
 
-#run the data through re/regex
-patternFile = open('items.dat','r').read().splitlines()
+#try an open the pattern file. 
+try:
+	patternFile = open('items.dat','r').read().splitlines()
+except:
+	print "There was an error opening the items.dat file"
+	sys.exit("Encountered some errors")
+#for each loop so we can process each specified regex
 for pattern in patternFile:
 	m = re.findall("'"+pattern+"'",data)
+	#m will return as true/false. Just need an if m:
 	if m:
 		#open output/results file...append because we are cool
 		outfile = open('RESULTS.txt','a')
@@ -23,6 +30,7 @@ for pattern in patternFile:
 		#close the file..or else
 		outfile.close()
 	else:
+		#Continue the loop if not a match so it can go on to the next sequence
 		continue
 
 	
